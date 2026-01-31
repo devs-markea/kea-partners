bee7d4e41e2c585ab955fe0a6012dc692a7ec964
saitki <85718309+saitki@users.noreply.github.com>
2026-01-30 19:06:11 -0500

    feat(article): refactor structure, rename Astro elements, and add new articles

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -8,6 +8,18 @@ import Mayaliah from './mayaliah.png';
 import TheRitz from './the-ritz-residences.png';
 import BannerFaena from './banner-faena-tulum.png';
 
+import NauticaBanner from './nautica-banner.png';
+import OneLagunaBanner from './onelaguna-banner.png';
+import AfloraBanner from './aflora-banner.png';
+import BayViewBanner from './bayviewgrand-banner.png';
+import MariettaBanner from './marietta-banner.png';
+import MayakobaBanner from './mayakoba-banner.png';
+import MgalleryBanner from './mgallery-banner.png';
+import RitzBanner from './ritz-banner.png';
+import StregisBanner from './stregis-banner.png';
+import ThompsonBanner from './thompson-banner.png';
+import InnaBanner from './inna-banner.png';
+
 export const images = {
     Logo: Logo,
     Faena: Faena,
@@ -19,4 +31,15 @@ export const images = {
     TheRitz: TheRitz,
     BannerFaena: BannerFaena,
 
+    NauticaBanner: NauticaBanner,
+    OneLagunaBanner: OneLagunaBanner,
+    AfloraBanner: AfloraBanner,
+    BayViewBanner: BayViewBanner,
+    MariettaBanner: MariettaBanner,
+    MayakobaBanner: MayakobaBanner,
+    MgalleryBanner: MgalleryBanner,
+    RitzBanner: RitzBanner,
+    StregisBanner: StregisBanner,
+    ThompsonBanner: ThompsonBanner,
+    InnaBanner: InnaBanner
 };
