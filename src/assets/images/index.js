e4e0cfb98c013b8aad7b28c010802527e2a4be59
saitki <85718309+saitki@users.noreply.github.com>
2026-02-24 13:43:33 -0500

    feat(routes): restructure routing, add destinations and new developments; add image resources (logo, banner, destinations)

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,31 +1,56 @@
 import Logo from './logo-brand.png';
-import Faena from './faena.png';
-import StRegis from './st-regis.png';
-import Nautica from './nautica.png';
-import Marietta from './marietta.png';
-import GranViaDelMar from './gran-via-del-mar.png';
-import Mayaliah from './mayaliah.png';
+
+
+import BannerFaena from '../images/banner/developments/banner-faena-tulum.png';
+import BannerNautica from '../images/banner/developments/nautica-banner.png';
+import BannerOneLaguna from '../images/banner/developments/onelaguna-banner.png';
+import BannerAflora from '../images/banner/developments/aflora-banner.png';
+import BannerBayView from '../images/banner/developments/bayviewgrand-banner.png';
+import BannerMarietta from '../images/banner/developments/marietta-banner.png';
+import BannerMayakoba from '../images/banner/developments/mayakoba-banner.png';
+import BannerMgallery from '../images/banner/developments/mgallery-banner.png';
+import BannerRitz from '../images/banner/developments/ritz-banner.png';
+import BannerStregis from '../images/banner/developments/stregis-banner.png';
+import BannerThompson from './thompson-banner.png';
+import BannerInna from '../images/banner/developments/inna-banner.png';
+
+import Aflora from '../images/Logo/developments/alfora.png';
+import BayView from '../images/Logo/developments/bay-view-grand.png';
+import Inna from '../images/Logo/developments/inna-beach-condos.png';
+import Mgallery from '../images/Logo/developments/MGallery.png';
+import OneLaguna from '../images/Logo/developments/one-laguna.png';
+import Thompson from '../images/Logo/developments/thompson.png';
+import Faena from '../images/Logo/developments/faena.png';
+import StRegis from '../images/Logo/developments/st-regis.png';
+import Nautica from '../images/Logo/developments/nautica.png';
+import Marietta from '../images/Logo/developments/marietta.png';
+import GranViaDelMar from '../images/Logo/developments/gran-via-del-mar.png';
+import Mayaliah from '../images/Logo/developments/mayaliah.png';
 import TheRitz from './the-ritz-residences.png';
-import BannerFaena from './banner-faena-tulum.png';
 
-import NauticaBanner from './nautica-banner.png';
-import OneLagunaBanner from './onelaguna-banner.png';
-import AfloraBanner from './aflora-banner.png';
-import BayViewBanner from './bayviewgrand-banner.png';
-import MariettaBanner from './marietta-banner.png';
-import MayakobaBanner from './mayakoba-banner.png';
-import MgalleryBanner from './mgallery-banner.png';
-import RitzBanner from './ritz-banner.png';
-import StregisBanner from './stregis-banner.png';
-import ThompsonBanner from './thompson-banner.png';
-import InnaBanner from './inna-banner.png';
 
-import Aflora from './alfora.png';
-import BayView from './bay-view-grand.png';
-import Inna from './inna-beach-condos.png';
-import Mgallery from './MGallery.png';
-import OneLaguna from './one-laguna.png';
-import Thompson from './thompson.png';
+import BannerTulum from '../images/banner/destinations/banner-tulum.png';
+import BannerCancun from '../images/banner/destinations/banner-cancun.png';
+import BannerRivieraMaya from '../images/banner/destinations/banner-riviera.png';
+import BannerMexicoCity from '../images/banner/destinations/banner-mexico-city.png';
+import BannerDubai from '../images/banner/destinations/banner-dubai.png';
+import BannerCostaMujeres from '../images/banner/destinations/banner-costa-mujeres.png';
+import BannerCostaSol from '../images/banner/destinations/banner-costa-sol.png';
+import BannerIslaMujeres from '../images/banner/destinations/banner-islas-mujeres.png';
+import BannerMiami from '../images/banner/destinations/banner-islas-mujeres.png';
+
+import BannerStRegis from '../images/banner/branded-residneces/banner-stregis.png';
+
+import IconBrandedResidenceStRegis from '../images/Logo/branded-residences/icon-branded-residences-stregis.png';
+import IconBrandedResidenceFaena from '../images/Logo/branded-residences/icon-branded-residences-faena.png';
+import IconBrandedResidenceLamborghini from '../images/Logo/branded-residences/icon-branded-residences-lamborghini.png';
+import IconBrandedResidenceMgallery from '../images/Logo/branded-residences/icon-branded-residences-mgallery.png';
+import IconBrandedResidenceMissoni from '../images/Logo/branded-residences/icon-branded-residences-missoni.png';
+import IconBrandedResidenceNautica from '../images/Logo/branded-residences/icon-branded-residences-nautica.png';
+import IconBrandedResidencePagani from '../images/Logo/branded-residences/icon-branded-residences-pagani.png';
+import IconBrandedResidenceRitzCarlton from '../images/Logo/branded-residences/icon-branded-residences-ritz-carlton.png';
+import IconBrandedResidenceThompson from '../images/Logo/branded-residences/icon-branded-residences-thompson.png';
+import IconBrandedResidenceTrumpTower from '../images/Logo/branded-residences/icon-branded-residences-trump-tower.png';
 
 export const images = {
     Logo: Logo,
@@ -38,17 +63,17 @@ export const images = {
     TheRitz: TheRitz,
     BannerFaena: BannerFaena,
 
-    NauticaBanner: NauticaBanner,
-    OneLagunaBanner: OneLagunaBanner,
-    AfloraBanner: AfloraBanner,
-    BayViewBanner: BayViewBanner,
-    MariettaBanner: MariettaBanner,
-    MayakobaBanner: MayakobaBanner,
-    MgalleryBanner: MgalleryBanner,
-    RitzBanner: RitzBanner,
-    StregisBanner: StregisBanner,
-    ThompsonBanner: ThompsonBanner,
-    InnaBanner: InnaBanner,
+    BannerNautica: BannerNautica,
+    BannerOneLaguna: BannerOneLaguna,
+    BannerAflora: BannerAflora,
+    BannerBayView: BannerBayView,
+    BannerMarietta: BannerMarietta,
+    BannerMayakoba: BannerMayakoba,
+    BannerMgallery: BannerMgallery,
+    BannerRitz: BannerRitz,
+    BannerStregis: BannerStregis,
+    BannerThompson: BannerThompson,
+    BannerInna: BannerInna,
 
     Aflora: Aflora,
     BayView: BayView,
@@ -56,4 +81,28 @@ export const images = {
     Mgallery: Mgallery,
     OneLaguna: OneLaguna,
     Thompson: Thompson,
+
+    BannerTulum: BannerTulum,
+    BannerCancun: BannerCancun,
+    BannerRivieraMaya: BannerRivieraMaya,
+    BannerMexicoCity: BannerMexicoCity,
+    BannerDubai: BannerDubai,
+    BannerCostaMujeres: BannerCostaMujeres,
+    BannerCostaSol: BannerCostaSol,
+    BannerIslaMujeres: BannerIslaMujeres,
+    BannerMiami: BannerMiami,
+
+    BannerStRegis: BannerStRegis,
+    IconBrandedResidenceStRegis: IconBrandedResidenceStRegis,
+    IconBrandedResidenceFaena: IconBrandedResidenceFaena,
+    IconBrandedResidenceLamborghini: IconBrandedResidenceLamborghini,
+    IconBrandedResidenceMgallery: IconBrandedResidenceMgallery,
+    IconBrandedResidenceMissoni: IconBrandedResidenceMissoni,
+    IconBrandedResidenceNautica: IconBrandedResidenceNautica,
+    IconBrandedResidencePagani: IconBrandedResidencePagani,
+    IconBrandedResidenceRitzCarlton: IconBrandedResidenceRitzCarlton,
+    IconBrandedResidenceThompson: IconBrandedResidenceThompson,
+    IconBrandedResidenceTrumpTower: IconBrandedResidenceTrumpTower,
+
+
 };
