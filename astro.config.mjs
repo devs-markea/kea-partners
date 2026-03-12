d9f19aa0a99f8854cdf2e93195eb1bb21c3eb684
saitki <85718309+saitki@users.noreply.github.com>
2026-03-12 15:17:46 -0500

    fix(ui): update reference and component HeroBannerSection, and add image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
     output: 'static',
@@ -9,4 +10,14 @@ export default defineConfig({
             nesting: true,
         }),
     ],
+    vite: {
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+                "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+                "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+                "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+            },
+        },
+    },
 });
